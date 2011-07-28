c970dbf4f89fa18ad86db60a2d64334f20a1d4e1
Pablo Recio Quijano <pablo@zocolab.es>
2011-07-28 13:35:44 +0200

    Added basic setup.py file

---
+++ setup.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2011 by Zocolab <pablo@zocolab.es>
+#
+# This software is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this software. If not, see <http://www.gnu.org/licenses/>.
+
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+setup(
+    name="django-qrcode",
+    version="0.1.0",
+    author="Pablo Recio Quijano",
+    author_email="pablo@zocolab.es",
+    description="Django application that provides simple templatetags to generate QR-codes",
+    long_description=(read('README')),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    ],
+    license="LGPL 3",
+    keywords="django qr code qrcode",
+    url='https://github.com/zocolab/django-qrcode',
+    packages=find_packages('.'),
+    include_package_data=True,
+    zip_safe=False,
+)
