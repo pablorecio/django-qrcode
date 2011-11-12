436a1a6e99fd272490e8a99913701f488bd0d843
Colin Bell <colin.bell@uwaterloo.ca>
2011-11-12 18:36:10 -0500

    Fixing... get_absolute_url

--- qrcode/templatetags/qr_tags.py
+++ qrcode/templatetags/qr_tags.py
@@ -62,6 +62,6 @@ def qr_from_contact(context, contact, size='M'):
 @register.inclusion_tag('qrcode/qr_tag.html', takes_context=True)
 def qr_from_object(context, obj, size='M'):
     domain = Site.objects.get_current().domain
-    path = obj.get_absolute_path()
+    path = obj.get_absolute_url()
     text = 'http://%s/%s'%(domain,path)
     return qr_from_text(context, text, size=size)
