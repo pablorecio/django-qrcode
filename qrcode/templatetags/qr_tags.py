174401773a10781f22c493c05906684addfcfb30
jadjay <jerome+github@rachetjay.fr>
2016-01-13 22:21:45 +0100

    Ajout de la propriété myclass, la propriété class n'est pas autorisé en python... ^^,

--- qrcode/templatetags/qr_tags.py
+++ qrcode/templatetags/qr_tags.py
@@ -21,7 +21,7 @@ register = template.Library()
 
 
 @register.inclusion_tag('qrcode/qr_tag.html', takes_context=True)
-def qr_from_text(context, text, size='M', class=''):
+def qr_from_text(context, text, size='M', myclass=''):
     if type(size) == type(0) or type(size) == type('') and size.isdigit():
         # this checks if it's an integer or a string with an integer
         actual_size = size
@@ -30,16 +30,17 @@ def qr_from_text(context, text, size='M', class=''):
         if not size.lower() in sizes_dict:
             size = 'm'
         actual_size = sizes_dict[size.lower()]
-    return {'text': text, 'size': actual_size, 'class': class}
+    myclass = ( 'class=%s' % myclass if myclass else '')
+    return {'text': text, 'size': actual_size, 'myclass': myclass}
 
 
 @register.inclusion_tag('qrcode/qr_tag.html', takes_context=True)
-def qr_from_mail(context, text, size='M', class=''):
-    return qr_from_text(context, text='mailto:%s' % text, size=size, 'class': class)
+def qr_from_mail(context, text, size='M', myclass=''):
+    return qr_from_text(context, text='mailto:%s' % text, size=size, myclass=myclass)
 
 
 @register.inclusion_tag('qrcode/qr_tag.html', takes_context=True)
-def qr_from_contact(context, contact, size='M', class=''):
+def qr_from_contact(context, contact, size='M', myclass=''):
     final_string = 'MECARD:'
     if contact['name']:
         final_string += 'N:%s;' % contact['name'].replace(' ', '+')
@@ -55,12 +56,12 @@ def qr_from_contact(context, contact, size='M', class=''):
     if contact['company']:
         final_string += 'ORG:%s;' % contact['company'].replace(
             ' ', '+')
-    return qr_from_text(context, text=final_string, size=size, 'class': class)
+    return qr_from_text(context, text=final_string, size=size, myclass=myclass)
 
 
 @register.inclusion_tag('qrcode/qr_tag.html', takes_context=True)
-def qr_from_object(context, obj, size='M', class=''):
+def qr_from_object(context, obj, size='M', myclass=''):
     domain = Site.objects.get_current().domain
     path = obj.get_absolute_url()
     text = 'http://%s%s' % (domain, path)
-    return qr_from_text(context, text, size=size, 'class': class)
+    return qr_from_text(context, text, size=size, myclass=myclass)
