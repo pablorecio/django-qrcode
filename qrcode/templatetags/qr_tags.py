039500740532cb08bfa4ec55fe17a352bc0bc427
Jérôme Avond <javond@adista.fr>
2016-01-21 16:16:31 +0100

    Create defaulturl to change faster the API source

--- qrcode/templatetags/qr_tags.py
+++ qrcode/templatetags/qr_tags.py
@@ -19,6 +19,8 @@ from django.contrib.sites.models import Site
 
 register = template.Library()
 
+defaulturl='src=https://chart.googleapis.com/chart?cht=qr&chs=%sx%s&chl=%s'
+
 
 @register.inclusion_tag('qrcode/qr_tag.html', takes_context=True)
 def qr_from_text(context, text, size='M', myclass=''):
@@ -31,7 +33,7 @@ def qr_from_text(context, text, size='M', myclass=''):
             size = 'm'
         actual_size = sizes_dict[size.lower()]
     myclass = ( 'class=%s' % myclass if myclass else '')
-    myurl = 'src=https://chart.googleapis.com/chart?cht=qr&chs=%sx%s&chl=%s' % (actual_size,actual_size,text)
+    myurl = defaulturl % (actual_size,actual_size,text)
     myalt = "alt=%s" % text
     return {'text': text, 'size': actual_size, 'myclass': myclass, 'myurl': myurl, 'myalt': myalt}
 
